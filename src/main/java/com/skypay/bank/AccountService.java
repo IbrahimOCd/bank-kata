c87930268266d5ebe88cf0a96bfc730082a948d4
IbrahimOCd <brahimoussiddi@gmail.com>
2025-02-25 10:18:36 +0000

    Create AccountService interface

--- src/main/java/com/skypay/bank/AccountService.java
+++ src/main/java/com/skypay/bank/AccountService.java
@@ -1,5 +1,26 @@
 package com.skypay.bank;
 
-public class AccountService {
+/**
+ * Interface defining the banking operations.
+ * This is the public interface that clients will use.
+ */
+public interface AccountService {
+    /**
+     * Makes a deposit to the account.
+     *
+     * @param amount The amount to deposit (positive integer)
+     */
+    void deposit(int amount);
 
+    /**
+     * Makes a withdrawal from the account.
+     *
+     * @param amount The amount to withdraw (positive integer)
+     */
+    void withdraw(int amount);
+
+    /**
+     * Prints a statement of all transactions for this account.
+     */
+    void printStatement();
 }
